131a167f6809610636de4495ef1b90024ddc96f1
kimsangjunv1 <ingyoe64@gmail.com>
2023-07-29 16:19:37 +0900

    Fix: 메인에서 사용되는 클래스명과 메뉴속 클래스명이 겹쳤던 문제 수정

--- src/components/layout/ArtistCont.jsx
+++ src/components/layout/ArtistCont.jsx
@@ -3,31 +3,22 @@ import { Link } from 'react-router-dom'
 
 function ArtistItem(props) {
   return (
-    <div className="artist unWidth">
-      <img
-        className="unWidth_artist"
-        src={`${props.artist.images.background}`}
-        alt=""
-      />
-      <div className="artistbox">
-        <h4>{props.artist.subtitle}</h4>
-        <div className="like">
+    <Link to="/artist" className="main_artist_item">
+      <div className="artist_info_container">
+        <h2 className="artist_name">{props.artist.subtitle}</h2>
+        <div className="artist_like_container">
           <img
             src="https://raw.githubusercontent.com/kimsangjunv1/-React-Pick-Music-Player/main/src/styles/img/like.svg"
             alt=""
           />
-          &nbsp; Likes {props.artist.key.slice(0, 3)}
+          <p>Likes {props.artist.key.slice(0, 3)}</p>
         </div>
-        {/* <audio
-          src={`${props.artist.hub.actions[1].uri}`}
-          type="audio/m4a"
-          controls
-          // autoPlay
-        >
-          <source src={`${props.artist.hub.actions[1].uri}`} type="audio/m4a" />
-        </audio> */}
       </div>
-    </div>
+      <img
+        src={`${props.artist.images.background}`}
+        alt={`${props.artist.subtitle}의 이미지`}
+      />
+    </Link>
   )
 }
 
@@ -35,11 +26,7 @@ const ArtistCont = ({ test }) => {
   // if (!test?.length) return <Loader />
   const [artistItem, setArtistItem] = useState([])
   const filterArtist = () => {
-    console.log(
-      '메인 아티스트 : ',
-      test.filter((item, index) => index >= 0 && index <= 4)
-    )
-    setArtistItem(test.filter((item, index) => index >= 0 && index <= 4))
+    setArtistItem(test.filter((item, index) => index >= 0 && index <= 7))
   }
 
   useEffect(() => {
@@ -47,18 +34,16 @@ const ArtistCont = ({ test }) => {
   }, [])
 
   return (
-    <div className="artist_cont unflex">
+    <div className="main_artist_cont">
       <div className="section_title">
         <p className="section_title_desc">지금 인기있는 아티스트는 누구?</p>
         <h2>Artist</h2>
       </div>
-      <div className="artistWrap">
-        <div className="artistinner unflex_artist">
-          <Link to="/artist" className="a_flex">
-            {artistItem.map((artist, index) => (
-              <ArtistItem key={index} artist={artist} />
-            ))}
-          </Link>
+      <div className="main_artist_inner">
+        <div className="main_artist_item_container">
+          {artistItem.map((artist, index) => (
+            <ArtistItem key={index} artist={artist} />
+          ))}
         </div>
         <Link to="/artist" className="artist_more">
           더보기
--- src/components/layout/RankCont.jsx
+++ src/components/layout/RankCont.jsx
@@ -5,33 +5,26 @@ import { Link } from 'react-router-dom'
 
 function RankingItem(props) {
   return (
-    <li className="item">
-      <div>{props.index + 1}</div>
-      <div>
-        <img src={`${props.ranking.images.coverart}`} alt="" />
-      </div>
-      <div>{props.ranking.title}</div>
-      <div>{props.ranking.subtitle}</div>
-      <div></div>
-    </li>
+    <Link to="/popular" className="ranking_item">
+      <p>{props.index + 1}</p>
+      <img src={`${props.ranking.images.coverart}`} alt="" />
+      <p>{props.ranking.title}</p>
+      <p>{props.ranking.subtitle}</p>
+    </Link>
   )
 }
 
 const RankCont = ({ test }) => {
   return (
-    <div className="ranking_cont unflex">
-      <div className="section_title">
+    <div className="main_ranking_cont">
+      <div className="main_ranking_title_container">
         <p className="section_title_desc">1위부터 10위까지 모아봤어요 XD</p>
         <h2>Top 10</h2>
       </div>
-      <div className="list_cont maxWidth">
-        <ul>
-          <Link to="/popular">
-            {test.map((ranking, index) => (
-              <RankingItem key={index} ranking={ranking} index={index} />
-            ))}
-          </Link>
-        </ul>
+      <div className="main_ranking_list_container">
+        {test.map((ranking, index) => (
+          <RankingItem key={index} ranking={ranking} index={index} />
+        ))}
       </div>
     </div>
   )
