773912c24f77a2e6b22e1403c66553db5bd7b9da
kimsangjunv1 <ingyoe64@gmail.com>
2023-11-13 16:33:33 +0900

    Improve: 푸터 로고 교체

--- src/components/include/Footer.jsx
+++ src/components/include/Footer.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from './../../styles/img/icon/footer_logo.svg'
 
 const Footer = () => {
   return (
     <footer id="footer">
       <div className="footer_inner">
-        <div>
-          <Link to="/" className="logo" href="">
-            <h1 className="ir">logo</h1>
-          </Link>
-        </div>
-        <div>
-          <p>프론트 : 김상준, 이영환&nbsp;&nbsp;|&nbsp;&nbsp;</p>
-          <p>디자인 : 김상준&nbsp;&nbsp;|&nbsp;&nbsp;</p>
-          <p>이메일 : to_before@naver.com</p>
-        </div>
+        <Link to="/" className="logo" href="">
+          {/* <h1 className="ir">logo</h1> */}
+          <img src={logo} alt="푸터로고" />
+        </Link>
       </div>
     </footer>
   )
